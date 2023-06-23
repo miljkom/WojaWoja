b5c1148241556f15dc33a8ea6555bedb18db55e6
Uros Spasenovic <uross@twodesperados.com>
2023-06-23 22:00:46 +0200

    Diagonal bullet

---
+++ Assets/Scripts/Bullet/DiagonalBulletMovement.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class DiagonalBulletMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float direction;
+    [SerializeField] private GameObject topBound;
+    [SerializeField] private GameObject bottomBound;
+    
+    private Transform _transform;
+    private float _topY;
+    private float _bottomY;
+    private void Awake()
+    {
+        _transform = transform;
+        _topY = topBound.transform.position.y;
+        _bottomY = bottomBound.transform.position.y;
+    }
+
+    private void Update()
+    {
+        if (direction > 0)
+        {
+            if (_transform.position.y > _topY)
+            {
+                direction = -1;
+                return;
+            }
+        }
+
+        if (direction < 0)
+        {
+            if (_transform.position.y < _bottomY)
+            {
+                direction = 1;
+                return;
+            }
+        }
+        transform.Translate(new Vector2(1,1 * direction).normalized  * Time.deltaTime * speed);
+    }
+}
--- Assets/Scripts/Player/ShootBullet.cs
+++ Assets/Scripts/Player/ShootBullet.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class ShootBullet : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bulletUp;
+    [SerializeField] private GameObject bulletDown;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
             Instantiate(bullet, transform.position, quaternion.identity);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(bullet, transform.position, quaternion.identity);
+            Instantiate(bulletUp, transform.position, quaternion.identity);
+            Instantiate(bulletDown, transform.position, quaternion.identity);
+        }
     }
 }
