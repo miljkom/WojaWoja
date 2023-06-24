0777b5d1c396c2bf3e28ac77e9ff57776363d8be
Uros Spasenovic <uross@twodesperados.com>
2023-06-24 15:43:37 +0200

    Totaly hard coded spawner

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,46 +3,45 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
-public class GameManager: Singleton<GameManager>
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] private List<GameObject> waypoints;
+    [SerializeField] private GameObject enemy;
+    [FormerlySerializedAs("shootingEnemy")] [SerializeField] private GameObject advanced1;
+    [FormerlySerializedAs("shooting3DEnemy")] [SerializeField] private GameObject advanced2;
+    [SerializeField] private GameObject advanced3;
+    [SerializeField] public GameObject voja;
+    public GameObject barricade;
+    public GameObject tiredness;
+    
+    private void Start()
     {
-
-        [SerializeField] private List<GameObject> waypoints;
-        [SerializeField] private GameObject enemy;
-        [SerializeField] private GameObject shootingEnemy;
-        [SerializeField] private GameObject shooting3DEnemy;
-        public GameObject voja;
-        public GameObject barricade;
-        public GameObject tiredness;
-
-        public bool spawn;
-
-        private void Start()
-        {
-            spawn = true;
-            Instantiate(barricade);
-        }
-
-        private void Update()
-        {
-            if (spawn)
-            {
-                StartCoroutine(SpawnEnemies());
-            }
-        }
-        
-        private IEnumerator SpawnEnemies()
-        {
-            spawn = false;
-            var enemyType = Random.Range(0, 10);
-            if(enemyType < 5)
-                Instantiate(enemy, waypoints[Random.Range(0,8)].transform.position, Quaternion.identity);
-            else if(enemyType < 8)
-                Instantiate(shootingEnemy, waypoints[Random.Range(0,8)].transform.position, Quaternion.identity);
-            else 
-                Instantiate(shooting3DEnemy, waypoints[Random.Range(0,8)].transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(2f);
-            spawn = true;
-        }
+        Instantiate(barricade);
     }
+
+    public void SpawnNormal()
+    {
+        Instantiate(enemy, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
+    }
+
+
+    public void SpawnAdvanced1()
+    {
+        Instantiate(advanced1, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
+    }
+    
+    
+    public void SpawnAdvanced2()
+    {
+        Instantiate(advanced2, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
+    }
+    
+    
+    public void SpawnAdvanced3()
+    {
+        Instantiate(advanced3, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
+    }
+}
