c10db12be504bddecaa5cf2d022a6ddcaaa3c974
Uros Spasenovic <uross@twodesperados.com>
2023-06-24 17:43:39 +0200

    Bullet goes through player line

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,56 +21,60 @@ public class GameManager : Singleton<GameManager>
     public GameObject tiredness;
     
     public bool spawn;
+
+    private int waypointsCount;
+
     private void Start()
+    {
+        spawn = true;
+        waypointsCount = waypoints.Count;
+        Instantiate(barricade);
+    }
+
+    private void Update()
+    {
+        if (spawn)
         {
-            spawn = true;
-            Instantiate(barricade);
+            StartCoroutine(SpawnEnemies());
         }
 
-        private void Update()
-        {
-            if (spawn)
-            {
-                StartCoroutine(SpawnEnemies());
-            }
+        chargerImage.fillAmount = Player.Instance.charger;
+    }
 
-            chargerImage.fillAmount = Player.Instance.charger;
-        }
-        
-        private IEnumerator SpawnEnemies()
-        {
-            spawn = false;
-            var enemyType = Random.Range(0, 10);
-            if(enemyType < 5)
-                Instantiate(enemy, waypoints[Random.Range(0,8)].transform.position, Quaternion.identity);
-            else if(enemyType < 8)
-                Instantiate(advanced1, waypoints[Random.Range(0,8)].transform.position, Quaternion.identity);
-            else 
-                Instantiate(advanced2, waypoints[Random.Range(0,8)].transform.position, Quaternion.identity);
-            yield return new WaitForSeconds(2f);
-            spawn = true;
-        }
+    private IEnumerator SpawnEnemies()
+    {
+        spawn = false;
+        var enemyType = Random.Range(0, 10);
+        if (enemyType < 5)
+            Instantiate(enemy, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+        else if (enemyType < 8)
+            Instantiate(advanced1, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+        else
+            Instantiate(advanced2, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        spawn = true;
+    }
 
-        public void SpawnNormal()
-        {
-            Instantiate(enemy, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
-        }
+    public void SpawnNormal()
+    {
+        Instantiate(enemy, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+    }
 
 
-        public void SpawnAdvanced1()
-        {
-            Instantiate(advanced1, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
-        }
-        
-        
-        public void SpawnAdvanced2()
-        {
-            Instantiate(advanced2, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
-        }
-        
-        
-        public void SpawnAdvanced3()
-        {
-            Instantiate(advanced3, waypoints[Random.Range(0, 8)].transform.position, Quaternion.identity);
-        }
+    public void SpawnAdvanced1()
+    {
+        Instantiate(advanced1, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+    }
+
+
+    public void SpawnAdvanced2()
+    {
+        Instantiate(advanced2, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+    }
+
+
+    public void SpawnAdvanced3()
+    {
+        Instantiate(advanced3, waypoints[Random.Range(0, waypointsCount)].transform.position, Quaternion.identity);
+    }
 }
