f3afb0d31f1ef0b0aadc6b6550d54680d06ae020
Uros Spasenovic <uross@twodesperados.com>
2023-06-24 16:47:09 +0200

    Bullet destroyer

--- Assets/DestroyBullet.cs
+++ Assets/DestroyBullet.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
 
 public class DestroyBullet : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
     {
         Destroy(col.gameObject);
     }
